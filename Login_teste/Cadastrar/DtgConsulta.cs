c23c82700b7ed73292efe0829a340cd059404e76
Adeilson Silva <adeilson.129@gmail.com>
2019-08-29 23:07:00 -0300

    Commit Corrigido

--- Login_teste/Cadastrar/DtgConsulta.cs
+++ Login_teste/Cadastrar/DtgConsulta.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Login_teste.Eminem;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Login_teste.Cadastrar
 {
     class DtgConsulta
     {
+        public static void CriandoDataAdapter()
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;port=3306;database=table_logar");
+            MySqlCommand comando = new MySqlCommand("select * from table_logar.cadastro;", conn);
+            MySqlDataAdapter dtg = new MySqlDataAdapter(comando);
+        }
     }
 }
--- Login_teste/View/dtgConsulta.Designer.cs
+++ Login_teste/View/dtgConsulta.Designer.cs
@@ -32,9 +32,9 @@
             this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtgConsul = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsul)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -59,36 +59,36 @@
             // salvarToolStripMenuItem
             // 
             this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
-            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.salvarToolStripMenuItem.Text = "Salvar";
             // 
             // sairToolStripMenuItem
             // 
             this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
-            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.sairToolStripMenuItem.Text = "Sair";
             // 
-            // dataGridView1
+            // dtgConsul
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 110);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 1;
+            this.dtgConsul.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgConsul.Location = new System.Drawing.Point(12, 106);
+            this.dtgConsul.Name = "dtgConsul";
+            this.dtgConsul.Size = new System.Drawing.Size(776, 309);
+            this.dtgConsul.TabIndex = 1;
             // 
             // dtgConsulta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtgConsul);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "dtgConsulta";
             this.Text = "dtgConsulta";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgConsul)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +100,6 @@
         private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgConsul;
     }
 }
